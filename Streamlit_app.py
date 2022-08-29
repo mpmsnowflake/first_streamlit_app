8a2ae3dcb24bfa5fc7110e37492df9ea747e4c69
mpmsnowflake <112407681+mpmsnowflake@users.noreply.github.com>
2022-08-29 10:31:03 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Patrents New Healty Diner')
 
-streamlit.header('Breaffast Menu')
-streamlit.text('Omegs 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('HArd-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omegs 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
