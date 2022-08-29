bf9f84cf3607fc3fca1e604f719194d5bd05fc92
mpmsnowflake <112407681+mpmsnowflake@users.noreply.github.com>
2022-08-29 10:27:16 -0500

    Update Streamlit_app.py
    mpm - added Breakfast menu

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Patrents New Healty Diner')
+
+streamlit.header('Breaffast Menu')
+streamlit.text('Omegs 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('HArd-Boiled Free-Range Egg')
