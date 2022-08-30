a9d85e3db70355c08b30f1155d5ae0e566a5becd
mpmsnowflake <112407681+mpmsnowflake@users.noreply.github.com>
2022-08-30 13:44:53 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -30,7 +30,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment -what does the next line do? - flattens Json data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
 # write your own comment - what does this do? - Puts data in to a table on screen
 streamlit.dataframe(fruityvice_normalized)
