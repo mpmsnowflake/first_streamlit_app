ebba3fb0764494e8eef84f09a23374e89ac6ea1a
mpmsnowflake <112407681+mpmsnowflake@users.noreply.github.com>
2022-08-30 15:52:20 -0500

    Update Streamlit_app.py
    changed

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -34,4 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? - Puts data in to a table on screen
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+import snowflake.connector 
