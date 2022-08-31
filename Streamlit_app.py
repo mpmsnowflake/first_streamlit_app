846a738bc30195a38293343077d1bdfb54d8d590
mpmsnowflake <112407681+mpmsnowflake@users.noreply.github.com>
2022-08-31 08:42:15 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -42,21 +42,27 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
+ 
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select* from fruit_load_list")
+    return my_cur.fetchall()
   
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
 #Stop command
 streamlit.stop()
-
-#import snowflake.connector 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select* from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
+                    
 #Newsection to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like add?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 
 #add row to PC_Rivery.Public.fruit_load_list
 my_cur.execute("insert into fruit_load_list values ('from Steamlit')")
+9'select * 
